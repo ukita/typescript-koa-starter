10ec37147317d65554fe7257ed880719627a243a
Bruno Ukita <brunoukita@gmail.com>
2018-06-20 13:00:30 -0300

    ⛑ add helmet dependency to make this app more secure

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,10 +2,11 @@ import compose from 'koa-compose'
 import bodyParser from 'koa-body'
 import compress from 'koa-compress'
 import logger from 'koa-pino-logger'
+import helmet from 'koa-helmet'
 import cors from '@koa/cors'
 
 import { app } from '../config'
 
 export default () => compose([logger({
   enabled: app.env !== 'test'
-}), compress(), bodyParser(), cors()])
+}), helmet(), compress(), bodyParser(), cors()])
